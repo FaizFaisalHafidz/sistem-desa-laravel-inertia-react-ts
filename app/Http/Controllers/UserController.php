4c9fd7b04ef33ad9d2427a2ba48574e3c5c9b9ad
FaizFaisalH <65413951+Faisalhfdz-25@users.noreply.github.com>
2024-12-23 13:19:39 +0700

    add: users management

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        $search = $request->input('search', '');
+        $users = User::query()
+            ->when($search, function ($query, $search) {
+                $query->where('name', 'like', "%{$search}%")
+                    ->orWhere('email', 'like', "%{$search}%");
+            })
+            ->paginate(5);
+
+        return Inertia::render('Users/Index', [
+            'users' => $users->items(),
+            'pagination' => [
+                'current_page' => $users->currentPage(),
+                'last_page' => $users->lastPage(),
+                'total' => $users->total(),
+            ],
+            'search' => $search,
+        ]);
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const colors = require('tailwindcss/colors');
-
+const withMT = require("@material-tailwind/react/utils/withMT");
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
@@ -270,4 +270,4 @@ module.exports = {
         },
     },
     plugins: [],
-};
+});
